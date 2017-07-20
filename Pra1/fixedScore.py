a26ffdd8f806190b32a5bdf83acb53bb0bbd1920
Woochul-Hyun <woochul.hyun@my.jcu.edu.au>
2017-07-20 16:44:27 +0800

    pra

--- Pra1/fixedScore.py
+++ Pra1/fixedScore.py
@@ -7,12 +7,11 @@ score = float(input("Enter score: "))
 
 if score < 0:
     print("Invalid score")
+elif score > 100:
+    print("Invalid score")
+elif score > 90:
+    print("Excellent")
+elif score > 50:
+    print("Passable")
 else:
-    if score > 100:
-        print("Invalid score")
-    if score > 90:
-        print("Excellent")
-    if score > 50:
-        print("Passable")
-if score < 50:
     print("Bad")
