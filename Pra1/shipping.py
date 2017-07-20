c1541a11155c888538ce1718a0cb11c16496075a
Woochul-Hyun <woochul.hyun@my.jcu.edu.au>
2017-07-20 17:40:54 +0800

    pra

--- Pra1/shipping.py
+++ Pra1/shipping.py
@@ -1,10 +1,12 @@
-num = float(input("Enter how many items: "))
-price = float(input("Enter price: "))
+num = int(input("Enter how many items: "))
+sum = 0
+for i in range(num):
+    price = float(input("Enter price for item: "))
+    sum += price
 
-total = num * price
-
-if total > 100:
-    result = total * 0.9
-    print(result)
-else:
+if sum > 100:
+    total = sum * 0.9
     print(total)
+else:
+    print(sum)
+
