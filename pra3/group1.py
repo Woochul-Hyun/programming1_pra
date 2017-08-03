7f84d4cd179f19b3e148566d32c461254a3e69f7
Woochul-Hyun <woochul.hyun@my.jcu.edu.au>
2017-08-04 01:50:26 +0800

    pra

--- pra3/group1.py
+++ pra3/group1.py
@@ -2,24 +2,25 @@ def get_password():
     password = input("Enter your password: ")
     return password
 
-
-def endcode(password):
-    encoded_password =""
-    for each in range(len(password)):
-        if each != len(password)-1:
-            encoded_password += ((ord(password[each]) -65)*2) +","
-        else:
-            encoded_password += str((ord(password[each]) -65)*2)
-        return encoded_password
-
 def print_to_file(encoded_password):
     out_file = open("secret.txt", "w")
     print(encoded_password, file=out_file)
     out_file.close()
 
-def main():
-    pw = get_password()
-    endcode = endcode(pw)
-    print_to_file(encoded)
 
+def encode(password):
+    encoded_password =""
+    for each in range(len(password)):
+        if each != len(password)-1:
+            encoded_password += str((ord(password[each]) -65)*2) +","
+        else:
+            encoded_password += str((ord(password[each]) -65)*2)
+    return encoded_password
+
+
+def main():
+    password = get_password()
+    encoded = encode(password)
+    print_to_file(encoded)
+    
 main()
