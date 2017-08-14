29ea41e67ce5aec8457c7012bf2157aa7b2c9f31
Woochul-Hyun <woochul.hyun@my.jcu.edu.au>
2017-08-15 00:22:26 +0800

    pra

--- pra4/group2.py
+++ pra4/group2.py
@@ -1,44 +1,28 @@
-"""
-Get into your preferred group, and work on the following:
-1. Construct a read_file() mrthod that reads in a string input as filename then
-return the list of data from file.
-   def read_file(filename):
-       .........
-       return data
-   calling:
-       data = read_file("data.txt")
-       data[0] = "2005-Q1, 0.00062"
-2. Second function that cleans up the data.
-   def clean_up(data):
-       return data
-
-    calling:
-       data = clean_up(data)
-       data[0] = ["2005", "Q1", 0.00062]
-3. finally display all data from main()
-
-"""
-
 def read_file():
-    data = open("mobile-data-usage.csv", "r")
+    FILENAME = "mobile-data-usage.csv"
+
+    file_pointer = open(FILENAME, "r")
+    data = file_pointer.read().splitlines()
     data.remove(data[0])
-    print(data)
+    file_pointer.close()
     return data
 
 
-def clean_up(data):
-    for each in data:
-        split_data = data.strip("-")
-        split_data2 = split_data[0].split(",")
-    return data
+
+def clean_file(data):
+
+    for i in data:
+        fixed_data = i.split()
+        new_parts = i.split('-')
+        new_parts2 = new_parts[1].split(',')
+        new_data = [new_parts[0], new_parts2[0], new_parts2[1]]
+
+        print(new_data)
+
+    return new_data
 
 
 def main():
-
-
-    FILENAME = "mobile-data-usage.csv"
     data = read_file()
-    data = clean_up(data)
-    print(split_data) # [[2005, "Q1", 0.00062], [2005, "Q1", 0.00062]]
-
+    clean_file(data)
 main()
